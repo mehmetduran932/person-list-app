178f3ff05cf9596281f5b3c9653aa6097671357c
mehmetduran932 <48024660+mehmetduran932@users.noreply.github.com>
2021-11-24 01:56:28 +0300

    key bug fixed

--- src/components/ListPerson.js
+++ src/components/ListPerson.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import { Flex, Box, Button } from '@chakra-ui/react'
-export default function ListPerson({users,removeItem,name,phone,email}) {
-    return (
-        <div>
-                  {users.map((p) => (
-                <Flex borderWidth="2px" marginTop="5px" borderColor="black">
-                  <Box p="4" bg="white.400" textAlign="left">
-                    <h1>Ad Soyad: {p.name}</h1>
-                    <h1>Telefon Numarası: {p.phone}</h1>
-                    <h1>E-mail: {p.email}</h1>
-                    <Button
-                      w="350px"
-                      colorScheme="red"
-                      onClick={() => removeItem(p)}
-                    >
-                      Kişiyi Sil
-                    </Button>
-                  </Box>
-                </Flex>
-              ))}
-        </div>
-    )
+export default function ListPerson({ users, removeItem, name, phone, email }) {
+  return (
+    <div>
+      {users.map((p) => (
+        <Flex
+          borderWidth="2px"
+          marginTop="5px"
+          borderColor="black"
+          key={p.phone}
+        >
+          <Box p="4" bg="white.400" textAlign="left">
+            <h1>Ad Soyad: {p.name}</h1>
+            <h1>Telefon Numarası: {p.phone}</h1>
+            <h1>E-mail: {p.email}</h1>
+            <Button w="350px" colorScheme="red" onClick={() => removeItem(p)}>
+              Kişiyi Sil
+            </Button>
+          </Box>
+        </Flex>
+      ))}
+    </div>
+  )
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,16 +4,7 @@ import '../../src/search.css'
 export default function Search() {
   return (
     <div>
-      <Flex marginTop="10px">
-        <Box>
-          <Input placeholder="Ad Soyad" w="220px" h="50px"/>
-        </Box>
-        <Box>
-          <Button w="150px" h="50px" colorScheme="blue" marginLeft="5px">
-            Ara
-          </Button>
-        </Box>
-      </Flex>
+       
     </div>
   )
 }
